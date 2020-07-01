b83204a7e44381ceed24c14dd3c59dc5f0a3544c
huangkai <h1770360848@outlook.com>
2020-07-01 20:51:54 +0800

    fix(user): user register schema tag problem

--- internal/app/v1/user/schema.go
+++ internal/app/v1/user/schema.go
@@ -7,9 +7,9 @@ import (
 // RegisterSchema is the user register schema.
 type RegisterSchema struct {
 	schema.BaseSchema
-	Username string `json:"username" validate:"required;max=127"`
-	Email    string `json:"email" validate:"required;email;max=127"`
-	Password string `json:"password" validate:"required;max=64"`
+	Username string `json:"username" binding:"required,max=127"`
+	Email    string `json:"email" binding:"required,email,max=127"`
+	Password string `json:"password" binding:"required,max=64"`
 }
 
 // Schema is the user return schema.
