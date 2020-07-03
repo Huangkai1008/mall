81370b53964ddc878a98d0e75eecef4a029332de
huangkai <h1770360848@outlook.com>
2020-07-03 17:17:22 +0800

    feat(model): add delete_time comment

--- internal/app/v1/user/model.go
+++ internal/app/v1/user/model.go
@@ -12,9 +12,10 @@ type User struct {
 	Username  string                `gorm:"type:varchar(127);not null;uniqueIndex:udx_username;comment:用户名" json:"username"`
 	Email     string                `gorm:"type:varchar(127);not null;uniqueIndex:udx_email;comment:邮箱" json:"email"`
 	Password  string                `gorm:"type:varchar(255);not null;comment:密码" json:"-"`
-	DeletedAt soft_delete.DeletedAt `gorm:"column:delete_time;not null;uniqueIndex:udx_username;uniqueIndex:udx_email"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:delete_time;not null;uniqueIndex:udx_username;uniqueIndex:udx_email;comment:删除时间"`
 }
 
+// ToSchema converts user model to user schema.
 func (u *User) ToSchema() Schema {
 	return Schema{
 		Username: u.Username,
