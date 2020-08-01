245d98f536fa572eef873a9ec85450756014b9ee
huangkai <h1770360848@outlook.com>
2020-08-01 18:26:56 +0800

    refactor(storage): minio provider

--- internal/pkg/storage/minio/minio.go
+++ internal/pkg/storage/minio/minio.go
@@ -1,27 +1,25 @@
 package minio
 
 import (
+	"github.com/google/wire"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/pkg/errors"
 
-	"mall/internal/pkg/config"
 	"mall/internal/pkg/constant"
 )
 
-type Options struct {
-	*config.Config
-}
-
 // New returns new minioClient instance with options.
-func New(opts *Options) (*minio.Client, error) {
-	minioClient, err := minio.New(opts.Endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(opts.AccessKeyID, opts.SecretAccessKey, ""),
-		Secure: opts.UseSSL,
-		Region: opts.Region,
+func New(o *Options) (*minio.Client, error) {
+	minioClient, err := minio.New(o.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(o.AccessKeyID, o.SecretAccessKey, ""),
+		Secure: o.UseSSL,
+		Region: o.Region,
 	})
 	if err != nil {
 		return nil, errors.Wrap(err, constant.MinioConfigError)
 	}
 	return minioClient, err
 }
+
+var ProviderSet = wire.NewSet(New, NewOptions)
