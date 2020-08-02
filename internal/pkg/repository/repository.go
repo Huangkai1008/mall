ba5f33a074d9b6d5cb240c18c7bbc6d528c8e28d
huangkai <h1770360848@outlook.com>
2020-08-02 20:41:14 +0800

    refactor(app): repository provider

--- internal/pkg/repository/repository.go
+++ internal/pkg/repository/repository.go
@@ -1,14 +1,19 @@
 package repository
 
+import metav1 "mall/pkg/meta/v1"
+
+// Repository is an interface for a repository.
 type Repository interface {
-	// GetAll return all records filter by the conditions.
-	GetAll(conditions interface{}) (records []*interface{}, err error)
+	// Get returns the record for the given id.
+	Get(id int) (record metav1.Resource, err error)
 	// GetOne return one record filter by the conditions.
-	GetOne(conditions interface{}) (record *interface{}, err error)
+	GetOne(conditions interface{}) (record metav1.Resource, err error)
+	// GetAll return all records filter by the conditions.
+	GetAll(conditions interface{}) (records []metav1.Resource, err error)
 
 	// Exist return one record does exist in table.
 	Exist(conditions interface{}) (bool, error)
 
 	// Create record.
-	Create(record interface{}) error
+	Create(record metav1.Resource) error
 }
