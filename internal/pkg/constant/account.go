be7f427cf823ea5f14fd09481adc24313ff01287
huangkai <h1770360848@outlook.com>
2020-10-01 22:13:21 +0800

    build: update go sum

--- internal/pkg/constant/account.go
+++ internal/pkg/constant/account.go
@@ -1,11 +1,6 @@
 package constant
 
 const (
-	AccountEmptyAuthHeader   = "请求头未携带token信息"
-	AccountInvalidAuthHeader = "请求头格式不合法"
-	AccountInvalidToken      = "Token不合法"
-	AccountTokenExpired      = "Token已过期"
-
 	AccountAlreadyExist       = "存在相同账号的用户"
 	AccountEmailAlreadyExist  = "存在相同邮箱的用户"
 	AccountNotExist           = "用户不存在"
